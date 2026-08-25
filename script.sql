9ff827a7bc6ea518ffd3bbf6fddea7a0dd2ba74d
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-25 10:55:09 -0300

    learn(ap01-dw) encerra o codigo 4
    - ja fizemos a carga de dados na tabela raw
    - criamos a tabela de staging
    - proximo passo sera a carga na staging passando pelo T (Transform)

--- script.sql
+++ script.sql
@@ -1,23 +1,25 @@
-DROP TABLE IF EXISTS raw.sales CASCADE;
-CREATE TABLE raw.sales(
-	invoice_id TEXT,
-	branch TEXT,
-	city TEXT,
-	customer_type TEXT,
-	gender TEXT,
-	product_line TEXT,
-	unit_price TEXT,
-	quantity TEXT,
-	tax_5pct TEXT,
-	total TEXT,
-	sale_date TEXT,
-	sale_time TEXT,
-	payment TEXT,
-	cogs TEXT,
-	gross_margin_percentage TEXT,
-	gross_income TEXT,
-	rating TEXT
-);
+SELECT * FROM raw.sales;
+
+-- DROP TABLE IF EXISTS raw.sales CASCADE;
+-- CREATE TABLE raw.sales(
+-- 	invoice_id TEXT,
+-- 	branch TEXT,
+-- 	city TEXT,
+-- 	customer_type TEXT,
+-- 	gender TEXT,
+-- 	product_line TEXT,
+-- 	unit_price TEXT,
+-- 	quantity TEXT,
+-- 	tax_5pct TEXT,
+-- 	total TEXT,
+-- 	sale_date TEXT,
+-- 	sale_time TEXT,
+-- 	payment TEXT,
+-- 	cogs TEXT,
+-- 	gross_margin_percentage TEXT,
+-- 	gross_income TEXT,
+-- 	rating TEXT
+-- );
 
 
 -- CREATE SCHEMA IF NOT EXISTS raw;
