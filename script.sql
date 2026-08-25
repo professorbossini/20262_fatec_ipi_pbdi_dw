86b7fb321204290468f07d6b2c43b2714e1a8b09
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-25 09:25:13 -0300

    learn(ap01-dw): cria a tabela raw

--- script.sql
+++ script.sql
@@ -1,6 +1,28 @@
-CREATE SCHEMA IF NOT EXISTS raw;
-CREATE SCHEMA IF NOT EXISTS staging;
-CREATE SCHEMA IF NOT EXISTS dw;
+DROP TABLE IF EXISTS raw.sales CASCADE;
+CREATE TABLE raw.sales(
+	invoice_id TEXT,
+	branch TEXT,
+	city TEXT,
+	customer_type TEXT,
+	gender TEXT,
+	product_line TEXT,
+	unit_price TEXT,
+	quantity TEXT,
+	tax_5pct TEXT,
+	total TEXT,
+	sale_date TEXT,
+	sale_time TEXT,
+	payment TEXT,
+	cogs TEXT,
+	gross_margin_percentage TEXT,
+	gross_income TEXT,
+	rating TEXT
+);
 
-SELECT schema_name
-FROM information_schema.schemata;
+
+-- CREATE SCHEMA IF NOT EXISTS raw;
+-- CREATE SCHEMA IF NOT EXISTS staging;
+-- CREATE SCHEMA IF NOT EXISTS dw;
+
+-- SELECT schema_name
+-- FROM information_schema.schemata;
