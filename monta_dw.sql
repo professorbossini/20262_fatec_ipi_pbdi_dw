788df66531c75f825ee79862d9aab8a86f54beb9
Rodrigo Bossini <professorbossini@gmail.com>
2026-09-01 09:43:01 -0300

    learn(ap01-dw): encerra codigo 12
    - dimensoes criadas
    - retomar com a carga dos dados de staging para dw

--- monta_dw.sql
+++ monta_dw.sql
@@ -1,4 +1,31 @@
-SELECT * FROM dw.dim_date;
+-- DROP TABLE IF EXISTS dw.dim_payment CASCADE;
+-- CREATE TABLE dw.dim_payment(
+--   payment_sk SERIAL PRIMARY KEY,
+--   payment_type VARCHAR(20) NOT NULL UNIQUE
+-- );
+
+-- DROP TABLE IF EXISTS dw.dim_customer CASCADE;
+-- CREATE TABLE dw.dim_customer(
+--   customer_sk SERIAL PRIMARY KEY,
+--   customer_type VARCHAR(10) NOT NULL,
+--   gender VARCHAR(10) NOT NULL,
+--   UNIQUE(customer_type, gender)
+-- );
+
+-- DROP TABLE IF EXISTS dw.dim_product CASCADE;
+-- CREATE TABLE dw.dim_product(
+--   product_sk SERIAL PRIMARY KEY,
+--   product_line VARCHAR(40) NOT NULL UNIQUE
+-- );
+
+--; DROP TABLE IF EXISTS dw.dim_branch CASCADE;
+-- CREATE TABLE dw.dim_branch(
+--   branch_sk SERIAL PRIMARY KEY,
+--   branch_code VARCHAR(200) NOT NULL UNIQUE,
+--   city VARCHAR(40) NOT NULL
+-- );
+
+-- SELECT * FROM dw.dim_date;
 
 -- INSERT INTO dw.dim_date
 -- SELECT
